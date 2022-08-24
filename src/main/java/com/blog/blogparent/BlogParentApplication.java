eeebd034f5b080c7568f75b2f226e69bd5fa0ad6
louie9985 <1633497269@qq.com>
2022-08-24 19:29:03 +0800

    Initial commit

---
+++ src/main/java/com/blog/blogparent/BlogParentApplication.java
@@ -0,0 +1,13 @@
+package com.blog.blogparent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogParentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogParentApplication.class, args);
+    }
+
+}
